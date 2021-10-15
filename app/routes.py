7ea0e159c1d7f8fed80f2a21c9db271473a8039c
Asli Athman <asliathman@gmail.com>
2021-10-15 14:05:27 -0400

    finished pt 1, all endpoint, one endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,42 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+class Planet:
+    def __init__(self, id, name, description, moons):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.moons = moons
+
+planets = [
+    Planet(1, "Mercury", "Rogue moon turned planet!", 0),
+    Planet(2, "Venus", "Hottest planet in our solar system ;-)", 0),
+    Planet(3, "Earth", "In her flop era", 1)
+]
+
+planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["GET"])
+def handle_planets():
+    planets_response = []
+    for planet in planets:
+        planets_response.append(
+            {"id" : planet.id,
+            "name" : planet.name,
+            "description" : planet.description,
+            "moons" : planet.moons,
+            }
+        )
+
+    return jsonify(planets_response)
+
+@planets_bp.route("/<id>", methods=["GET"])
+def handle_planet(id):
+    id = int(id)
+    for planet in planets:
+        if planet.id == id:
+            return {
+                "id" : planet.id,
+                "name" : planet.name,
+                "description" : planet.description,
+                "moons" : planet.moons,
+                }
