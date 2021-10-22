a03b77288fcede2b747c9c3a6b18f49833284bcc
Ivette Fernandez <ivette.diana.fernandez@gmail.com>
2021-10-22 13:29:45 -0500

    added routes for reading one planet, reading all planets, and creating a planet

--- app/routes.py
+++ app/routes.py
@@ -1,57 +1,52 @@
-from flask import Blueprint, jsonify
-import requests
-
-class Planet:
-    def __init__(self, id, name, description, moons):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.moons = moons
-
-planets = [
-    Planet(1, "Mercury", "Rogue moon turned planet!", 0),
-    Planet(2, "Venus", "Hottest planet in our solar system ;-)", 0),
-    Planet(3, "Earth", "In her flop era", 1)
-]
+from app import db
+from app.models.planet import Planet
+from flask import Blueprint, jsonify, make_response, request
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
-@planets_bp.route("", methods=["GET"])
+@planets_bp.route("", methods=["POST", "GET"])
 def handle_planets():
-    planets_response = []
-    for planet in planets:
-        planets_response.append(
-            {"id" : planet.id,
-            "name" : planet.name,
-            "description" : planet.description,
-            "moons" : planet.moons,
-            }
-        )
-
-    return jsonify(planets_response)
-
-@planets_bp.route("/<id>", methods=["GET"])
-def handle_planet(id):
-    id = int(id)
-    for planet in planets:
-        if planet.id == id:
-            return {
+    if request.method == "GET":
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planets_response.append({
                 "id" : planet.id,
                 "name" : planet.name,
                 "description" : planet.description,
                 "moons" : planet.moons,
-                }
+            })
+        return jsonify(planets_response)
 
-def get_response(planet):
-    path = "https://api.le-systeme-solaire.net/rest/bodies/"
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_planet = Planet(name=request_body["name"], description=request_body["description"], moons=request_body["moons"])
 
-    query_params = {
-    "filter[]" : f"englishName,eq,{planet}"
-    }
+        db.session.add(new_planet)
+        db.session.commit()
 
-    response = requests.get(path, params=query_params).json()
-    print(response)
-    print()
+        return make_response(f"Planet {new_planet.name} was successfully created", 201)
 
-get_response("mars")
-get_response("venus")
+@planets_bp.route("/<id>", methods=["GET"])
+def handle_planet(id):
+    planet = Planet.query.get(id)
+    return {
+        "id" : planet.id,
+        "name" : planet.name,
+        "description" : planet.description,
+        "moons" : planet.moons,
+        }
+
+# def get_response(planet):
+#     path = "https://api.le-systeme-solaire.net/rest/bodies/"
+
+#     query_params = {
+#     "filter[]" : f"englishName,eq,{planet}"
+#     }
+
+#     response = requests.get(path, params=query_params).json()
+#     print(response)
+#     print()
+
+# get_response("mars")
+# get_response("venus")
