18c71c872b7beab444daf2fb245fece269d52f2f
Asli Athman <asliathman@gmail.com>
2021-10-27 14:17:45 -0400

    test setup, env variables, created get planet with no records test + passed

---
+++ tests/test_routes.py
@@ -0,0 +1,7 @@
+def test_get_planet_with_no_records(client):
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body == []
+
